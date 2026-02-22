d927def5b5cace24e60aa4644954b43a889e4acc
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-02-22 18:20:52 +0530

    Add factorial calculation program
    Implement factorial calculation using a for loop.

---
+++ iteratin_statement/factorial of a number.py
@@ -0,0 +1,21 @@
+# Program to calculate factorial of a number using a for loop
+
+# Take input from the user
+num = int(input("Enter a number: "))
+
+# Check if the number is negative
+if num < 0:
+    print("Factorial is not defined for negative numbers")
+
+# Factorial of 0 is 1
+elif num == 0:
+    print("Factorial of 0 is 1")
+
+else:
+    factorial = 1  # Initialize factorial to 1
+    
+    # Multiply numbers from 1 to num
+    for i in range(1, num + 1):
+        factorial = factorial * i
+    
+    print("Factorial of", num, "is", factorial)
