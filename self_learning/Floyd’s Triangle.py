f4b10e2aa8395d27bff7f8fe6c0d0d982f2f0e89
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-02-22 19:34:20 +0530

    Add Floyd's Triangle implementation
    Implement Floyd's Triangle to print numbers in a triangular format.

---
+++ self_learning/Floyd’s Triangle.py
@@ -0,0 +1,16 @@
+# Number of rows
+n = 5
+num = 1  # Starting number
+
+for i in range(1, n + 1):
+    for j in range(i):
+        print(num, end=' ')
+        num += 1
+    print()  # Move to next row
+
+output
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
