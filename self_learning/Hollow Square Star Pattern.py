f2e3e69d810efb30c439e3b4b0888568016569c1
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-02-22 19:53:58 +0530

    Create Hollow Square Star Pattern.py

---
+++ self_learning/Hollow Square Star Pattern.py
@@ -0,0 +1,19 @@
+# Number of rows and columns
+n = 5
+
+for i in range(n):
+    for j in range(n):
+        # Print star for borders
+        if i == 0 or i == n - 1 or j == 0 or j == n - 1:
+            print('*', end='')
+        else:
+            print(' ', end='')  # Hollow space
+    print()  # Move to next row
+
+output
+
+*****
+*   *
+*   *
+*   *
+*****
