02390b1679cc9f6096c0cf881a4723ba5663e4f8
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-02-22 19:23:34 +0530

    Add hollow pyramid star pattern script
    Implement a hollow pyramid star pattern printing algorithm.

---
+++ self_learning/Hollow Pyramid Star Pattern.py
@@ -0,0 +1,22 @@
+# Number of rows
+n = 5
+
+for i in range(1, n + 1):
+    # Print leading spaces
+    print(' ' * (n - i), end='')
+    
+    # Print stars and spaces
+    for j in range(1, 2 * i):
+        if j == 1 or j == 2 * i - 1 or i == n:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    
+    print()  # Move to next line
+
+output
+    *
+   * *
+  *   *
+ *     *
+*********
