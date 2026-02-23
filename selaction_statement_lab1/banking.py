bc753c374cab22f2d0fbc853fb7d92a6d18af582
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-02-23 20:50:37 +0530

    Update banking.py

--- selaction_statement_lab1/banking.py
+++ selaction_statement_lab1/banking.py
@@ -8,3 +8,8 @@ if amount > 200000 and account_age < 6 and international == "yes":
     print("Transaction Flagged for Manual Verification.")
 else:
     print("Transaction is Normal.")
+#output
+#Enter transaction amount: 300000
+#Enter account age in months: 3
+# the transaction international? (yes/no): yes
+#Transaction Flagged for Manual Verification.
