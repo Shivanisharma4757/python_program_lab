29782cdd45b31c540d03403fac7c154da26201e5
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-02-23 21:55:09 +0530

    Create Set_ in_ Python.py

---
+++ 23_Feb_class_lab_work/Set_ in_ Python.py
@@ -0,0 +1,26 @@
+# ------------------------------------------
+# Program to demonstrate Set in Python
+# ------------------------------------------
+
+# Creating a set with repeated elements
+set1 = {45, 67, 89, 45, 67, 89, 34, 90, 45, 59, 120, 34, 90, 45, 59, 120}
+
+# Displaying the set
+print("Set is:")
+print(set1)
+
+# ------------------------------------------
+# Inserting an element into the set
+# ------------------------------------------
+
+# Adding 34 to the set
+set1.add(34)
+
+print("After inserting 34 into the set:")
+print(set1)
+
+#output
+#Set is:
+#{34, 67, 89, 45, 90, 59, 120}
+#After inserting 34 into the set:
+#{34, 67, 89, 45, 90, 59, 120}
