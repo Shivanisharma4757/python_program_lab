de915f926743a7711acd347e409f8687f76daae9
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-02-23 21:13:28 +0530

    Update Alphabet Triangle.py

--- self_learning/Alphabet Triangle.py
+++ self_learning/Alphabet Triangle.py
@@ -6,9 +6,9 @@ for i in range(1, n + 1):
         print(chr(65 + j), end='')  # 65 is ASCII of 'A'
     print()  # Move to next line
 
-output
-A
-AB
-ABC
-ABCD
-ABCDE
+#output
+#A
+#AB
+#ABC
+#ABCD
+#ABCDE
