92a1b7a81cfdce7fbe7518c9ce0181bb2f21b914
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-02-24 14:00:30 +0530

    Create number_in_range.py

---
+++ 24_Feb_class_lab_work_100question_python/number_in_range.py
@@ -0,0 +1,13 @@
+# Check whether number lies within a given range
+
+num = int(input("Enter number: "))
+
+# Range condition
+if 1 <= num <= 100:
+    print("Number is in range")
+else:
+    print("Number is out of range")
+
+#output
+#Enter number: 45
+#Number is in range
