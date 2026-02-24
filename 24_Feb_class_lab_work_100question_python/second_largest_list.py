2835312ed49e3138a178362ac632e751dede0ef5
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-02-24 14:47:59 +0530

    Create second_largest_list.py

---
+++ 24_Feb_class_lab_work_100question_python/second_largest_list.py
@@ -0,0 +1,12 @@
+# Program to find the second largest number in a list
+
+numbers = [10, 45, 23, 89, 12]        # Create a list of numbers
+
+numbers.sort()                        # Sort the list
+
+second_largest = numbers[-2]          # Get second largest element
+
+print("Second largest element:", second_largest)  # Display result
+
+# Output Example
+# Second largest element: 45
