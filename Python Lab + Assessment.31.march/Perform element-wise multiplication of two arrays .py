b1f6e36384e0c9f4b040e24ac0671dd159c3a8aa
Shivanisharma4757 <shivani.sharma.mca.2025@miet.ac.in>
2026-03-31 11:05:53 +0530

    Fix newline at end of Perform element-wise multiplication script
    Added a newline at the end of the file for proper formatting.

---
+++ Python Lab + Assessment.31.march/Perform element-wise multiplication of two arrays .py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Input: Two NumPy arrays
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([5, 6, 7, 8])
+
+# Element-wise multiplication
+result = arr1 * arr2
+
+# Output
+print("Resultant array:", result)
+
+#output
+#Resultant array: [ 5 12 21 32]
